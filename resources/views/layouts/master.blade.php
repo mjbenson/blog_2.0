0574376f927d1856d68d32c768d0e41a332f716c
= Matt James Benson <= matt.benson@intelligentclaims.com>
2017-06-07 12:06:58 +0100

    Working product

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,7 +24,7 @@
 
 @include ('layouts.nav')
 
-<div class = "container"></div>
+<div class = "container">
     <div class="blog-header">
         <h1 class="blog-title">The Bootstrap Blog</h1>
         <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>

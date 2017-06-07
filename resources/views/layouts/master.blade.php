801b710214596b1f0f44870838c3151ac21363a1
= Matt James Benson <= matt.benson@intelligentclaims.com>
2017-06-07 11:46:26 +0100

    Broken code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,45 +3,40 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Album example for Bootstrap</title>
+    <title>Blog Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="/css/album.css" rel="stylesheet">
+    <link href="blog.css" rel="stylesheet">
+
 </head>
 
 <body>
 
-<div class="collapse bg-inverse" id="navbarHeader">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8 py-4">
-                <h4 class="text-white">About</h4>
-                <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-            </div>
-            <div class="col-sm-4 py-4">
-                <h4 class="text-white">Contact</h4>
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                    <li><a href="#" class="text-white">Like on Facebook</a></li>
-                    <li><a href="#" class="text-white">Email me</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-
 @include ('layouts.nav')
 
+<div class="blog-header">
+    <h1 class="blog-title">The Bootstrap Blog</h1>
+    <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+</div>
+
 <div class = "container">
-    @yield ('content')
+    <div class = "row">
+
+        @yield ('content')
+
+        @include ('layouts.sidebar')
+
+    </div>
 </div>
 
 @include ('layouts.footer')

67a10aec9ea2dec85c36b59a96b1c7fd5478555c
= Matt James Benson <= matt.benson@intelligentclaims.com>
2017-06-08 15:58:05 +0100

    Working add comment section with browser and server validation

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,17 @@ class Post extends Model
 
     }
 
+    public function addComment($body){
+
+        $this->comments()->create(compact('body'));
+
+//        Comment::create([
+//
+//            'body'=>$body,
+//            'post_id'=> $this->id
+//
+//        ]);
+
+    }
+
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,4 +10,36 @@
 
         @include('posts.comments')
 
+        <div class ="card">
+
+            <div class ="card-block">
+
+                <form method ="POST" action="/posts/{{$post->id}}/comments">
+
+                    {{ csrf_field() }}
+
+                    <div class ="form-group">
+
+                        <textarea name ="body" placeholder="Your comment here" class ="form-control" required></textarea>
+
+                    </div>
+
+                    <div class ="form-group">
+
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+
+                    </div>
+
+                </form>
+
+                @include('layouts.errors')
+
+            </div>
+
+        </div>
+
+    </div>
+
+    @include('layouts.sidebar')
+
 @endsection
